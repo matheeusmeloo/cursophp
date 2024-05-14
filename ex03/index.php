d29af6c4588c2248e8da0db70653a211a356673a
Matheus Melo <rmelomatheus@gmail.com>
2024-05-14 14:01:34 -0300

    Update index.php

--- ex03/index.php
+++ ex03/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="https://www.php.net/favicon.ico?v=2">
+    <link rel="icon" type="image/x-icon" href="https://sail.faunalia.eu/fancy-index/icons/php.svg">
     <title>Teste tipos primitivos</title>
 </head>
 <body>
