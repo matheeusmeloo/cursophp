960910a60723d789526aa7b518d1d268de3d6b93
Matheus Melo <rmelomatheus@gmail.com>
2024-06-20 10:07:16 -0300

    Link favicon

--- ex05/cad.php
+++ ex05/cad.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado</title>
+    <link rel="icon" type="image/x-icon" href="https://sail.faunalia.eu/fancy-index/icons/php.svg">
     <link rel="stylesheet" href="../style.css">
+    <title>Resultado</title>
 </head>
 <body>
     <header>
