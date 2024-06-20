b558ebd6191f976629b1f48b14e345c2da02f298
Matheus Melo <rmelomatheus@gmail.com>
2024-06-20 12:07:39 -0300

    Uso de aritmética, com parênteses e sem parênteses

---
+++ ex06/index.php
@@ -0,0 +1,5 @@
+<?php
+    //$res = 50 / 2 + 3 ** 2;
+    $res = 50 / (2 + 3) ** 2; // apenas mudando e colocando o parenteses vai alterar o valor do resultado final, seguindo a regra aritimética  onde aspas primeiro, elevado segundo e divisão, multiplicação e resto divisão em terceiro.
+    echo "O resultado é $res";
+?>
