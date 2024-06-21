1d5165f182b9f51830191c2e40383ea676b13445
Matheus Melo <rmelomatheus@gmail.com>
2024-06-21 10:10:12 -0300

    Desafio 05
    Esse aqui foi mais tranquilo já que o de consumir a API do Banco Central foi mais complexo. Utilizei um fórum de Portugal que um cara deu uma solução que me ajudou muito!

--- desafios01/05_analisar_num_real/analisar_num_real.php
+++ desafios01/05_analisar_num_real/analisar_num_real.php
@@ -12,10 +12,10 @@
         <h1>Analisador de Número Real</h1>
     </header>
         <section>
-            <form action="result_moeda_v2.php" method="get">
-                <label for="numero">Quantos R$ você tem na carteira: </label>
-                <input type="number" name="numero" id="idnumero">
-                <input type="submit" value="Converter">
+            <form action="result_analisar_num_real.php" method="get">
+                <label for="numero">Número Real: </label>
+                <input type="number" name="numero" id="idnumero" step="0.00000001" min="0.00000001"> <!-- Utilizado step e min para poder ser validado a inserção de número com vírgula -->
+                <input type="submit" value="Analisar">
             </form>
             <a href="javascript:history.go(-1)"><button type="button">⬅️ Voltar</button></a>
         </section>
--- desafios01/05_analisar_num_real/result_analisar_num_real.php
+++ desafios01/05_analisar_num_real/result_analisar_num_real.php
@@ -5,20 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="https://sail.faunalia.eu/fancy-index/icons/php.svg">
     <link rel="stylesheet" href="../../style.css">
-    <title>Conversor Moeda v1</title>
+    <title>Analisador de Número Real</title>
 </head>
 <body>
     <header>
-        <h1>Conversor de Moeda (Básico)</h1>
+        <h1>Analisador de Número Real</h1>
     </header>
     <main>
         <?php
-            // //var_dump($_REQUEST);// $_GET $_POST $_COOKIE - O request é basicamente a junção de todos estes itens
-            // $n = $_GET["numero"] ?? "Valor inválido";
-            // $cotacao = 5.45; //cotação dólar dia 20/06/2024
-            // $convert = round($n / $cotacao, 2);//resultado, utilizei a função round e chamei o número 2 pra dizer que queria mais dois números após a virgula.
-            // echo "<p>Seus R$ $n equivalem a <strong>US$ $convert</strong></p>";
-            // echo "<p><strong>Cotação fixa de R$ $cotacao </strong> informada diretamente no código fonte.</p>";
+            $n = $_GET["numero"] ?? "Valor inválido";
+            // $frac = substr(strpbrk($n, '.,'), 1); // aqui ele vira uma string
+            $frac = (int) substr(strpbrk($n, '.,'), 1); // assim o valor permanece como número
+            // $i = "";
+            // $f = "";
+
+            echo "<p>Analisando o número <strong>$n</strong> informado pelo usuário:</p>";
+            echo "<ul>
+                    <li>
+                        A parte inteira do número é: <strong>".intval($n)."</strong>
+                    </li>";
+            echo "<li>
+                    A parte fracionária do número é: <strong>0,$frac</strong>
+                  </li>
+                  </ul>";
         ?>
          <a href="javascript:history.go(-1)"><button type="button">⬅️ Voltar</button></a>
     </main>
