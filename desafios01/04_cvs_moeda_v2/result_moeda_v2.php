56c884a60075be41a00c25c75ae25917c2441d32
Matheus Melo <rmelomatheus@gmail.com>
2024-06-21 08:48:06 -0300

    Desafio 04
    Consumindo informações da API do Banco Central com json.

--- desafios01/04_cvs_moeda_v2/result_moeda_v2.php
+++ desafios01/04_cvs_moeda_v2/result_moeda_v2.php
@@ -13,31 +13,26 @@
     </header>
     <main>
         <?php
-         $hoje = (new DateTime())->format('m-d-Y');
-         $uri = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=\''.$hoje.'\'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao';
-         $response = file_get_contents($uri);
-         $jsonData = json_decode($response);
-         //var_dump($jsonData); //Aqui é para exibir os valores
-
-             if (isset($jsonData->value) && count($jsonData->value) > 0) {
-                 // Extrair os dados
-                 $cotacao = $jsonData->value[0];
-                 $cotacao->cotacaoCompra;
-                 $n = $_GET["numero"] ?? "Valor inválido";
-                 $convert = round($n / $cotacao, 2);//resultado, utilizei a função round e chamei o número 2 pra dizer que queria mais dois números após a virgula.
-                 echo "<p>Seus R$ $n equivalem a <strong>US$ $convert</strong></p>";
-                 echo "<p><strong>Cotação importada do Banco Central do Brasil</p>";
-
-             } else {
-                 echo "Nenhuma cotação encontrada para a data: " . $hoje . ".\n";
-             }
-
-        //var_dump($_REQUEST);// $_GET $_POST $_COOKIE - O request é basicamente a junção de todos estes itens
-        
+        //  $hoje = "06-20-2024";
+          $hoje = (new DateTime())->format('m-d-Y'); // O Banco Central não trás informações do dólar nos dias: SEXTA, SÁBADO E DOMINGO).
+          $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=\''.$hoje.'\'&$top=100&$format=json&$select=cotacaoCompra';
+         // Obter o conteúdo da URL
+            $response = file_get_contents($url);
+                // Decodificar o JSON
+                $jsonData = json_decode($response);
+                    // Verificar se há dados na resposta
+                    if (isset($jsonData->value) && count($jsonData->value) > 0) {
+                        // Extrair os dados
+                        $cotacao = $jsonData->value[0];
+                         $n = $_GET["numero"] ?? "Valor inválido";
+                         $convert = round($n / $cotacao->cotacaoCompra, 2);//resultado, utilizei a função round e chamei o número 2 pra dizer que queria mais dois números após a virgula.
+                         echo "<p>Seus R$ $n equivalem a <strong>US$ $convert</strong></p>";
+                         echo "<p><strong>Cotação de R$ ".round($cotacao->cotacaoCompra, 2)." importada do Banco Central do Brasil</p>";
+                    } else {
+                        echo "Nenhuma cotação encontrada para a data: " . $hoje . ".\n";
+                    }
         ?>
          <a href="javascript:history.go(-1)"><button type="button">⬅️ Voltar</button></a>
-            <span></span> 
-        </a>
     </main>
 </body>
 </html>
